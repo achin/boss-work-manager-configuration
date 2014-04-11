3a933dd35f9fc03c2801031a0c343c40e01547a6
Alex Chin <achin@42six.com>
2014-04-11 08:29:01 -0400

    Add clj-xpath dependency.

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/achin/boss-work-manager-configuration"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.github.kyleburton/clj-xpath "1.4.3"]])
